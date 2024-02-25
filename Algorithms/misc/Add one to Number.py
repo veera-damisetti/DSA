bee935125cbeda25614809095b4fb8a86c30e6df
VeerababuDamisetti <62247232+VeerababuDamisetti@users.noreply.github.com>
2024-02-25 18:23:00 +0530

    Create Add one to Number.py

---
+++ Algorithms/misc/Add one to Number.py
@@ -0,0 +1,27 @@
+You are given a non-negative number represented as an array of digits.
+
+Example: 123 is represented as [1,2,3]
+
+You must add 1 to the number ( increment the number represented by the digits ) and return the required array or list.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+
+If the vector has [1, 2, 3], the returned vector should be [1, 2, 4],
+
+as 123 + 1 = 124.
+
+Some notes:
+
+Your returned array must not have any leading zeros. Example:
+Input:
+[0,0,2,1]
+ 
+Ouput:
+[2,2] // no leading zeros
+Maximum size of the array is 300 so avoid converting it to a string or an integer. Instead, try to think of what happens when 1 is added to the number.
+
+
+
+  
