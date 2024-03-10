3fe7455927c05743afcc134f5f5e09e542a040d7
Veerabhadrarao Damisetti <veerababudamisetti5555@gmail.com>
2024-03-10 14:43:48 +0530

    Create Binary Search.py

---
+++ Algorithms/Searching/Binary Search.py
@@ -0,0 +1,26 @@
+'''
+Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing the search interval in half.
+The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
+
+Time Complexity:
+
+ O(log n)
+
+'''
+A=list(map(int,input().split()))
+key = int(input('Enter the key to search for'))
+found = False
+low=0
+high = len(A)-1
+mid = (high + low )//2
+while A[mid]!= key and high >= low: 
+    mid = (high + low )//2
+    if A[mid]==key:
+        found = True
+        break
+    elif A[mid]>key:
+        high = mid-1
+    else:
+        low=mid+1
+        
+print(found)
