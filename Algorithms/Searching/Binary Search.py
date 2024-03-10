77e2599f3e14a2d33fc1ac12a8c5193a28ffb97c
Veerabhadrarao Damisetti <veerababudamisetti5555@gmail.com>
2024-03-10 15:00:48 +0530

    Update Binary Search.py

--- Algorithms/Searching/Binary Search.py
+++ Algorithms/Searching/Binary Search.py
@@ -2,6 +2,9 @@
 Binary Search is defined as a searching algorithm used in a sorted array by repeatedly dividing the search interval in half.
 The idea of binary search is to use the information that the array is sorted and reduce the time complexity to O(log N). 
 
+
+"Divide and Conquer" strategy
+
 Time Complexity:
 
  O(log n)
